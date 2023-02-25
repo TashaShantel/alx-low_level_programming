20ada88e2cff4aab5923e76e16a4a7863ee98c55
TashaShantel <natashaonono@gmail.com>
2023-02-25 13:49:27 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,13 +14,17 @@ int d, p;
 
 for (d = '0'; d <= '9'; d++)
 {
-putchar(d);
-for (p = d + 1; p <= '9'; p++)
+for (p = d+1; p <= '9'; p++)
+{
+putchar(d+'0');
+putchar(p+'0');
+if (d != 8 || p != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
+}
 return (0);
 }
